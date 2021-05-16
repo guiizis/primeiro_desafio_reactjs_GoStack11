d6e2267b1979589627cbef2ae17fa5394280912a
guilherme Moda <guilherme.moda@iv2.com.br>
2021-05-16 17:24:46 -0300

    implementacao de funcao de deletar e de salvar repos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+
+
+
   async function handleAddRepository() {
-    // TODO
+    api.post(`/repositories`, {
+      title: `project created at : ${Date.now()}`,
+      url: "https://github.com/CodeSeven",
+      techs: [],
+
+    }).then(response => {
+      setProjects([response.data, ...projects])
+
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`)
+      .then(_ => {
+
+        const indexOfDeletedRepo = projects.findIndex(repository => repository.id == id)
+
+        projects.splice(indexOfDeletedRepo, 1)
+
+        setProjects([...projects]) //spread to create a new array to change the state
+      })
+      .catch(error => console.error(error))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {projects.map((project, index) => (
+          <li key={project.id} >
+            <h4>{project.title}</h4>
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
